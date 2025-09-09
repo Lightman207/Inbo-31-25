137f8fcf25b279105f78619ba8466bfa006af40b
Daniil Lightman <105736320+Lightman207@users.noreply.github.com>
2025-09-09 11:45:18 +0300

    Create practice1-2.cpp

---
+++ practice1-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main()
+{
+  
+  // Практика 1 и 2
+    
+    long long a, b;
+    std::cout << "Enter the number A and then B \n";
+    std::cin >> a >> b;
+    std::cout << "Addition: " << a + b << "\n";
+    std::cout << "Subtraction: " << a - b << "\n";
+    std::cout << "Multiplication: "  << a * b << "\n";
+    std::cout << "Division: " << a / b << "\n";
+}
